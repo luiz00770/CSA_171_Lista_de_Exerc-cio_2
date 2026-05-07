c5dd577a872436195c3a8af7e9c345220765597a
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-05-07 16:31:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imc_genêro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Você se identifica com o genêro feminino ou masculino: ");
+            string s = Console.ReadLine();
+
+            Console.Write("Qual é seu peso: ");
+            double p = double.Parse(Console.ReadLine());
+
+            Console.Write("Qual é a sua altura: ");
+            double a = double.Parse(Console.ReadLine());
+
+            double r = p / (a * a);
+
+            if (s == "masculino")
+            {
+                if (r < 20)
+                {
+                    Console.WriteLine("Abaixo do peso");
+                }
+                else
+                {
+                    if (r < 25)
+                    {
+                        Console.WriteLine("Peso ideal");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Acima do peso");
+                    }
+
+                }
+            }
+
+            if (s == "feminino")
+            {
+                if (r < 19)
+                {
+                    Console.WriteLine("Abaixo do peso");
+                }
+                else
+                {
+                    if (r < 24)
+                    {
+                        Console.WriteLine("Peso ideal");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Acima do peso");
+                    }
+
+                }
+            }
+
+        }
+    }
+}
